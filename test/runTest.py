80b37d00abc47f55b409a7cd9d1d422f42a32eec
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-05-29 19:40:19 +0900

    Check for assertion errors.

--- test/runTest.py
+++ test/runTest.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 import pathlib
 from unittest import TestLoader
 from unittest import TextTestRunner
 
 
 def main():
-    loader = TestLoader()
-    test = loader.discover(str(pathlib.Path(__file__).resolve().parent / 'suite'))
-    runner = TextTestRunner()
-    runner.run(test)
+    testResult = TextTestRunner().run(
+        TestLoader().discover(
+            str(pathlib.Path(__file__).resolve().parent / 'suite')
+        )
+    )
+
+    sys.exit(0 if testResult.wasSuccessful() else 1 )
+
 
 if __name__ == '__main__':
     main()
