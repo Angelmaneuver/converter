30c064a5c63d33fc323d7983f943ae5cdb87725e
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-05-30 19:32:20 +0900

    Code Review.

--- src/kamipro.py
+++ src/kamipro.py
@@ -1,5 +1,103 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import flow.kamipro as flow
+import pathlib
+from typing import NamedTuple, Tuple, Any
+from prefect import Flow, task
 
-flow.execute()
+import task.util.absorber as absorber
+import task.util.yml as yml
+from task.dataSource.excel import ExcelDataSourceClass
+from task.kamipro import KamiProConverterClass
+
+
+class Definition(NamedTuple):
+    header:    Any
+    names:     Any
+    skipRows:  int
+    sortBy:    Any
+    ascending: Any
+
+
+class Condition(NamedTuple):
+    path:             str
+    sheet:            str
+    rarity:           str
+    additionalRarity: str
+    output:           str
+
+
+@task(name='Convert')
+def convert(parameter: Tuple[Definition, Condition]) -> Tuple[str, Condition]:
+    definition, condition = parameter
+
+    converter = KamiProConverterClass(
+        condition.rarity,
+        condition.additionalRarity
+    )
+
+    dataSource = ExcelDataSourceClass(
+        io=condition.path,
+        sheet_name=condition.sheet,
+        header=definition.header,
+        names=definition.names,
+        skiprows=definition.skipRows,
+        sort_by=definition.sortBy,
+        ascending=definition.ascending
+    )
+
+    for row in dataSource.fetch():
+        converter.convert(row)
+
+    return (converter.get_result(), condition)
+
+
+@task(name='Output Converted Data')
+def output(parameter: Tuple[str, Condition]):
+    converted, condition = parameter
+
+    with open(condition.output, 'wt', encoding='utf-8_sig') as fw:
+        fw.write(converted)
+
+
+def main():
+    with Flow("神姫プロジェクトエピソ－ドメモ変換") as flow:
+        config = yml.read(
+            absorber.resource(__file__, 'config', 'kamipro.yml')
+        )
+        definition = Definition(
+            header=config['header'],
+            names=config['names'],
+            skipRows=config['skipRows'],
+            sortBy=config['sortBy'],
+            ascending=config['ascending']
+        )
+
+        config = yml.read(pathlib.Path().joinpath('config', 'kamipro.yml'))
+        baseDestination = pathlib.Path(config['output']['destination'])
+        prefix = config['output']['prefix']
+        suffix = config['output']['suffix']
+        extension = '.' + config['output']['extension']
+        parameter: list[Tuple[Definition, Condition]] = list()
+        for input in config['inputs']:
+            parameter.extend(
+                list(map(lambda condition: (definition, Condition(
+                    path=input['path'],
+                    sheet=condition['sheet'],
+                    rarity=condition['rarity'],
+                    additionalRarity=condition['additionalRarity'],
+                    output=baseDestination.joinpath(
+                        prefix + condition['convertedName'] + suffix +
+                        extension
+                    )
+
+                )), input['conditions']))
+            )
+
+        converted = convert.map(parameter)
+        output.map(converted)
+
+    flow.run()
+
+
+if __name__ == '__main__':
+    main()
